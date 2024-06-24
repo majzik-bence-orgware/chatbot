ecfea8649bfc96159051d16f3a1675db393e13df
Bence <bence.majzik96@gmail.com>
2024-06-24 14:39:53 +0200

    Added files

---
+++ frontend/src/app.tsx
@@ -0,0 +1,368 @@
+import React, { useEffect, useRef, useState } from "react";
+import { ThreadMessage } from "openai/resources/beta/threads/messages/messages";
+import Markdown from 'markdown-to-jsx';
+import { Cv } from "./cv";
+import axios from "axios";
+
+const BASE_URL = process.env.BASE_URL;
+if (!BASE_URL) {
+    throw new Error("Missing BASE_URL environment variable.");
+}
+
+export function App() {
+
+    const threadId = useRef<string | undefined>(undefined);
+    const scrollContainer = useRef<HTMLDivElement>(null);
+    const [runId, setRunId] = useState<string | undefined>(undefined);
+    const [messages, setMessages] = useState<ThreadMessage[]>([]);
+    const [message, setMessage] = useState<string>("");
+    const [chatbotVisible, setChatbotVisible] = useState<boolean>(true);
+
+    //
+    // Creates a new message thread, if there isn't one already.
+    //
+    async function createThread(): Promise<void> {
+
+        if (threadId.current === undefined) {
+            //
+            // Try and reload from local storage.
+            //
+            const storedThreadId = localStorage.getItem("threadId");
+            if (storedThreadId) {
+                threadId.current = storedThreadId;
+                await updateMessages();
+            }
+        }
+
+
+        if (threadId.current !== undefined) {
+            // Already have a thread.
+            return;
+        }
+
+        //
+        // Requests a new thread from the backend.
+        //
+        const { data } = await axios.post(`${BASE_URL}/chat/new`);
+        threadId.current = data.threadId;
+
+        //
+        // Save the thread id in local storage.
+        //
+        localStorage.setItem("threadId", threadId.current!);
+    }
+
+    //
+    // Starts a new chat thread.
+    //
+    function onResetThread() {
+        threadId.current = undefined;
+        setMessages([]);
+        setMessage("");
+        setRunId(undefined);
+        scrollContainer.current!.scrollTop = 0;
+        localStorage.removeItem("threadId");
+
+        createThread()
+            .catch(err => {
+                console.error(`Failed to create message thread.`);
+                console.error(err);
+            });
+    }
+
+    //
+    // Adds a message to the chat.
+    //
+    async function sendMessage(text: string): Promise<void> {
+        if (runId !== undefined) {
+            // Already running.
+            return;
+        }
+
+        //
+        // Sends the message to the backend.
+        //
+        const { data } = await axios.post(`${BASE_URL}/chat/send`, {
+            threadId: threadId.current,
+            text,
+        });
+
+        setRunId(data.runId);
+    }
+
+    //
+    // Sends the message the user has typed to the AI.
+    //
+    async function onSendMessage(): Promise<void> {
+        const messageToSend = message.trim();
+        setMessage(""); // Clear for the next message.
+        await sendMessage(messageToSend);
+    }
+
+    //
+    // Updates messages in the UI.
+    //
+
+    async function updateMessages(): Promise<void> {
+
+        const { data } = await axios.post(`${BASE_URL}/chat/list`, {
+            threadId: threadId.current,
+            runId: runId,
+        });
+
+        const { messages, status } = data;
+
+        messages.reverse(); // Reverse so the newest messages are at the bottom.
+        setMessages(messages);
+
+        if (runId) {
+            if (status === "completed") {
+                setTimeout(() => {
+                    // The run has finished.
+                    setRunId(undefined);
+                }, 5000); // Give it some time to finish up.
+            }
+        }
+    }
+
+    //
+    // Gets the role name for a message.
+    //
+    function getRoleName(role: string): string {
+        if (role === "user") {
+            return "Felhasználó";
+        }
+        else if (role === "assistant") {
+            return "OrgwareAI";
+        }
+        else {
+            return role;
+        }
+    }
+
+    //
+    // Render a chunk of text as paragraphs.
+    //
+    function renderText(text: string, role: string) {
+        return text.split("\n").map((line, index) => {
+            return (
+                <div key={index} className="leading-relaxed">
+                    {index === 0
+                        && <span
+                            className="block font-bold text-gray-700"
+                        >
+                            {getRoleName(role)}
+                        </span>
+                    }
+                    <Markdown>{line}</Markdown>
+                </div>
+            );
+        });
+    }
+
+    useEffect(() => {
+        createThread()
+            .catch(err => {
+                console.error(`Failed to create message thread.`);
+                console.error(err);
+            });
+    }, []);
+
+    useEffect(() => {
+
+        if (runId === undefined) {
+            return;
+        }
+
+        const timer = setInterval(() => {
+            updateMessages();
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+
+    }, [runId]);
+
+    useEffect(() => {
+        //
+        // Scroll to the end.
+        //
+        scrollContainer.current!.scrollTop = scrollContainer.current!.scrollHeight;
+
+    }, [messages]);
+
+    //
+    // Toggles visibility of the chatbot.
+    //
+    function onToggleChatbot() {
+        setChatbotVisible(!chatbotVisible);
+    }
+
+    //
+    // Handles the enter key.
+    //
+    async function onKeyDown(event: React.KeyboardEvent<HTMLInputElement>): Promise<void> {
+        if (event.key === "Enter") {
+            // Send message on enter.
+            await onSendMessage();
+        }
+    }
+    return (
+        <div>
+            {chatbotVisible && <div
+                style={{
+                    boxShadow: "0 0 #0000, 0 0 #0000, 0 1px 2px 0 rgb(0 0 0 / 0.05)",
+                    maxHeight: "80%",
+                    maxWidth: "80%",
+                }}
+                className="flex flex-col fixed bottom-[calc(4rem+1.5rem)] right-0 mr-4 bg-gray-50 p-6 pt-2 rounded-lg border border-[#e5e7eb] w-[440px] h-[600px]"
+            >
+
+                {/* <!-- Heading --> */}
+                <div className="flex flex-col space-y-1.5 pb-6">
+                    <h2 className="font-semibold text-lg tracking-tight">Kérdezz az OrgwareAI-tól!</h2>
+                    <p className="text-xs text-[#6b7280] leading-3">Powered by Open AI (ChatGPT) téjákoztató szövege...</p>
+                </div>
+
+                {/* <!-- Chat Container --> */}
+                <div
+                    ref={scrollContainer}
+                    className="flex-grow overflow-y-auto mb-6 pr-4"
+                >
+                    {/* <!-- Chat Message AI --> */}
+                    <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
+                        <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+                            <div className="rounded-full bg-gray-100 border p-1">
+                                <svg stroke="none" fill="black" strokeWidth="1.5"
+                                    viewBox="0 0 24 24" aria-hidden="true" height="20" width="20" xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                        d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </span>
+                        <p className="leading-relaxed">
+                            <span className="block font-bold text-gray-700">OrgwareAI </span> Default szöveg is lehet minden új csetelés megkezdésénél. (Pl hogy a tudásbázisa a kézikönyveinkre alapul...)
+                        </p>
+                    </div>
+
+                    {messages.map((message, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className="flex gap-3 my-4 text-gray-600 text-sm flex-1"
+                            >
+                                <span
+                                    className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8"
+                                >
+                                    <div className="rounded-full bg-gray-100 border p-1">
+                                        {message.role === "assistant"
+                                            && <svg stroke="none" fill="black" strokeWidth="1.5"
+                                                viewBox="0 0 24 24" aria-hidden="true" height="20" width="20" xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z">
+                                                </path>
+                                            </svg>
+                                        }
+                                        {message.role === "user"
+                                            && <svg stroke="none" fill="black" strokeWidth="0"
+                                                viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z">
+                                                </path>
+                                            </svg>
+                                        }
+                                    </div>
+                                </span>
+
+                                <div className="flex flex-col">
+                                    {message.content.map((content, index) => {
+                                        if (content.type === "text") {
+                                            return renderText(content.text.value, message.role);
+                                        }
+                                        else {
+                                            return undefined;
+                                        }
+                                    })}
+                                </div>
+
+                            </div>
+                        );
+                    })}
+
+                    {/* Cool progress indicators: https://loading.io/css/ */}
+                    {runId !== undefined
+                        && <div className="lds-ring">
+                            <div></div><div></div><div></div><div></div>
+                        </div>
+                    }
+                </div>
+
+                {/* <!-- Input box  --> */}
+                <div className="flex flex-col">
+                    <div className="flex items-center pt-0">
+                        <div className="flex items-center justify-center w-full space-x-2">
+                            <input
+                                className="flex h-12 w-full rounded-md border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#] disabled:cursor-not-allowed disabled:opacity-50 text-[#000000] focus-visible:ring-offset-2"
+                                placeholder="Üzenet"
+                                value={message}
+                                onChange={e => setMessage(e.target.value)}
+                                disabled={runId !== undefined}
+                                onKeyDown={onKeyDown}
+                            />
+
+                            <button
+                                className="inline-flex items-center justify-center rounded-md text-sm font-medium text-[#ffffff] disabled:pointer-events-none disabled:opacity-50 bg-[#cc0133] hover:bg-[#111827E6] h-10 px-4 py-2"
+                                onClick={onSendMessage}
+                                disabled={runId !== undefined}
+                            >
+                                Kérdez
+                            </button>
+
+                        </div>
+                    </div>
+
+                    <div className="text-sm ml-3 pt-3 pr-1 text-gray-500">
+                        Példa: Hogyan tudom megváltoztatni a jelszavam?
+                    </div>
+                </div>
+                <div
+                    className="absolute top-[20px] right-[20px] cursor-pointer"
+                    onClick={onToggleChatbot}
+                >
+                    <svg width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"></path>
+                    </svg>
+                </div>
+                <div
+                    className="absolute top-[20px] right-[60px] cursor-pointer"
+                    onClick={onResetThread}
+                >
+                    <svg width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"></path>
+                    </svg>
+                </div>
+
+            </div>
+            }
+            <button
+                className="fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 border rounded-full w-16 h-16 bg-[#cc0133] hover:bg-gray-700 m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900"
+                type="button" aria-haspopup="dialog" aria-expanded="false" data-state="closed"
+                onClick={onToggleChatbot}
+            >
+                <svg xmlns=" http://www.w3.org/2000/svg" width="30" height="40" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                    className="text-white block border-gray-200 align-middle">
+                    <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" className="border-gray-200">
+                    </path>
+                </svg>
+            </button>
+
+            <Cv />
+        </div>
+    );
+}
---
+++ frontend/src/cv.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Cv() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <p className="text-4xl">
+                <span>OrgwareAI</span>
+            </p>
+        </div>
+    );
+}
